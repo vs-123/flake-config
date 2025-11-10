ab446b884e03a4b165a04991255bb00af203c2da
vs-123 <>
2025-11-10 13:56:31 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My Flake Config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nebnix = lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [./configuration.nix];
+      };
+    };
+
+    homeConfigurations = {
+      neb = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."aarch64-linux";
+        modules = [./home.nix];
+      };
+    };
+  };
+}
