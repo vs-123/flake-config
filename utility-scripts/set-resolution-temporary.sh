ed776fd7723da70348027d213b4f78b8b4c7ce70
vs-123 <>
2025-11-13 08:26:39 +0000

    set-resolution-temporary.sh: rewrote the entire script
    original one was very buggy

--- utility-scripts/set-resolution-temporary.sh
+++ utility-scripts/set-resolution-temporary.sh
@@ -1,65 +1,65 @@
 #!/bin/bash
 
+# "xrandr --newmode $modeline_res"
+# "xrandr --addmode $display_output $modename"
+# "xrandr --output $display_output $modename"
+
 echo "======================================"
 echo "===== RESOLUTION SETTER BY vs-12 ====="
 echo "======================================"
 
 echo "* This is temporary"
+echo ""
+echo "[PROMPT FROM SCRIPT] Enter your desired resolution & refresh rate in the following format:"
+echo "==> <width_px> <height_px> <refresh_rate_hz>"
+echo ""
 
-is_it_right="n"
 cvt_args=""
-
-while [[ "$(echo "$is_it_right" | tr "[:upper:]" "[:lower:]")" != "y" ]]; do
-echo "Enter the resolution and refresh rate you need in the following format:"
-echo "> <width> <height> <refresh_rate_hz>"
-echo ""
-read -p "> " cvt_args
-cvt_args="$(echo "$cvt_args" | tr -s " ")"
-echo "Is this right?"
-echo ">>> $cvt_args"
-read -p "(y/n) ? " is_it_right
-echo ""
+yesno="n"
+while [[ ! "$yesno" =~ [yY] ]]; do
+   read -p "==> " cvt_args
+   echo "[CONFIRMATION]"
+   echo ">>> $cvt_args"
+   echo ""
+   read -p "Is this right? (y/n) " yesno
 done
 
 modeline_cmd="cvt $cvt_args"
-modeline_res_raw="$(eval $modeline_cmd)"
-modeline_res="$(echo $modeline_res_raw | grep 'Modeline' | sed "s/Modeline //")"
-modename="$(echo $modeline_res | awk "{print $2}")"
+modeline_res=$(eval $modeline_cmd)
+modeline_res=$(echo "$modeline_res" | tail -n 1)
 
-display_output=""
-is_it_right="n"
-while [[ "$(echo "$is_it_right" | tr "[:upper:]" "[:lower:]")" != "y" ]]; do
-xrandr
-echo "Available display outputs are given above."
-echo "Choose the display output on which you'd like to apply the new mode."
-read -p "> " display_output
-echo "Is this right?"
-echo ">>> $display_output"
-read -p "(y/n) ? " is_it_right
-echo ""
-done
+modeline_fmt=$(echo "$modeline_res" | sed -r 's/(.*)Modeline//g')
+modename=$(echo "$modeline_fmt" | awk '{print $1}')
 
-xrandr_cmd_1="xrandr --newmode $modeline_res"
+display_output=$(eval xrandr | grep -v 'disconnected' | grep 'connected' | awk '{print $1}')
+echo "Display Output: $display_output"
+read -p "Is this your display output? (y/n) " yesno
+
+if [[ ! "$yesno" =~ [yY] ]]; then
+   while [[ ! "$yesno" =~ [yY] ]]; do
+      eval xrandr
+      read -p "Please enter your display output: " display_name
+      echo "Display Output: $display_output"
+      read -p "Is this your display output? (y/n) " yesno
+   done
+fi
+
+xrandr_cmd_1="xrandr --newmode $modeline_fmt"
 xrandr_cmd_2="xrandr --addmode $display_output $modename"
-xrandr_cmd_3="xrandr --output $display_output $modename"
+xrandr_cmd_3="xrandr --output $display_output --mode $modename"
 
-eval $xrandr_cmd_1
-eval $xrandr_cmd_2
 
-read -p "Would you like to apply the mode right now? (y/n) " yes_no
+echo "I am going to run the following commands:"
+echo "   " $xrandr_cmd_1
+echo "   " $xrandr_cmd_2
+echo "   " $xrandr_cmd_3
+read -p "Should I continue? (y/n) " yesno
 
-if [[ "$(echo "$yes_no" | tr "[:upper:]" "[:lower:]")" == "y" ]]; then
+if [[ "$yesno" =~ [yY] ]]; then
+   eval $xrandr_cmd_1     
+   eval $xrandr_cmd_2     
    eval $xrandr_cmd_3
 fi
 
-echo "======================================"
-echo "===== INSTRUCTIONS FOR PERMANENT ====="
-echo "======================================"
-echo "* Paste the following three lines into ~/.xprofile"
-echo "$xrandr_cmd_1"
-echo "$xrandr_cmd_2"
-echo "$xrandr_cmd_3"
-
 echo "THE END"
-
 
